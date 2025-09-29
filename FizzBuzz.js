84073e5b3b6b0bd9e2cf221726fc31922fd9819f
Silvus <hugo.combe.84@gmail.com>
2025-09-29 15:20:21 +0200

    feat : Etape 1.5

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,3 +1,5 @@
+const readline = require('readline');
+
 function fizzBuzz(number = 100) {
   let out = '';
   if (number % 3 === 0) out += 'Fizz';
@@ -5,5 +7,9 @@ function fizzBuzz(number = 100) {
   return out.toString() || number.toString();
 }
 
-
-console.log(fizzBuzz());
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+rl.question('Entrez un nombre: ', (answer) => {
+  const num = Number(answer);
+  console.log(fizzBuzz(num));
+  rl.close();
+})
