c92b4563acb7a1614368d98e7206b95b2160b61a
Crtmetton <crtmetton@gmail.com>
2025-09-29 16:00:29 +0200

    Etape 3

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,15 +1,70 @@
 const readline = require('readline');
 
+const FIZZ_DIVISOR = 3;
+const BUZZ_DIVISOR = 5;
+
+/**
+ * Converts a number to FizzBuzz representation
+ */
 function fizzBuzz(number = 100) {
-  let out = '';
-  if (number % 3 === 0) out += 'Fizz';
-  if (number % 5 === 0) out += 'Buzz';
-  return out.toString() || number.toString();
+  let result = '';
+  if (number % FIZZ_DIVISOR === 0) result += 'Fizz';
+  if (number % BUZZ_DIVISOR === 0) result += 'Buzz';
+  return result || number.toString();
 }
 
-const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-rl.question('Entrez un nombre: ', (answer) => {
-  const num = Number(answer);
-  console.log(fizzBuzz(num));
-  rl.close();
-})
+/**
+ * Creates and returns a readline interface
+ */
+function createReadlineInterface() {
+  return readline.createInterface({ 
+    input: process.stdin, 
+    output: process.stdout 
+  });
+}
+
+/**
+ * Processes user input and converts it to a number
+ */
+function processUserInput(userInput) {
+  return Number(userInput);
+}
+
+/**
+ * Displays the FizzBuzz result
+ */
+function displayResult(result) {
+  console.log(result);
+}
+
+/**
+ * Handles the complete user interaction flow
+ */
+function handleUserInteraction() {
+  const readlineInterface = createReadlineInterface();
+  
+  readlineInterface.question('Entrez un nombre: ', (userInput) => {
+    const inputNumber = processUserInput(userInput);
+    const fizzBuzzResult = fizzBuzz(inputNumber);
+    displayResult(fizzBuzzResult);
+    readlineInterface.close();
+  });
+}
+
+/**
+ * Displays welcome message for the FizzBuzz game
+ */
+function displayWelcomeMessage() {
+  console.log('FizzBuzz Game');
+}
+
+/**
+ * Main function - entry point of the application
+ */
+function main() {
+  displayWelcomeMessage();
+  handleUserInteraction();
+}
+
+// Application entry point
+main();
